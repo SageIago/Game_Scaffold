9111df6ec61d0f33ab6f8721d6e31adacee429a0
Sage_Iago <158774357+SageIago@users.noreply.github.com>
2024-07-02 01:18:56 +0100

    Building a Custom Hook to Fetch the Games

---
+++ src/Hooks/useGames.ts
@@ -0,0 +1,38 @@
+//DECLARATION OF THE PACKAGES
+import { useState, useEffect } from "react";
+import apiClient from "../api-client";
+import { CanceledError } from "axios";
+
+// TO DEFINE THE SHAPE OF THE GAME OBJECT
+interface Game {
+  id: number;
+  title: string;
+}
+
+const useGames = () => {
+  // THIS IS FOR THE GAMES ARRAY
+  const [games, setGames] = useState<Game[]>([]);
+
+  // THIS IS FOR THE ERROR ARRAY
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    // CREATING A CONTROLLER FOR CONTROLLING THE OBJECTS
+    const CONTROLLER = new AbortController();
+
+    apiClient
+      .get<Game[]>("/games", { signal: CONTROLLER.signal })
+      .then((response) => setGames(response.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return; // THIS SEMI-COLON IS VERY IMPORTANT.....
+        setError(err.message);
+      });
+
+    // TO DO THE CLEANUP EFFECT
+    return () => CONTROLLER.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
-import apiClient from "../api-client";
-import { Text } from "@chakra-ui/react";
 
-// TO DEFINE THE SHAPE OF THE GAME OBJECT
-interface Game {
-  id: number;
-  title: string;
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "../Hooks/useGames";
+
 
 const GameGrid = () => {
-  // THIS IS FOR THE GAMES ARRAY
-  const [games, setGames] = useState<Game[]>([]);
-
-  // THIS IS FOR THE ERROR ARRAY
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    apiClient
-      .get<Game[]>("/games")
-      .then((response) => setGames(response.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  // We use the newly created hook
+    const {games, error} = useGames()
   return (
     <>
       {error && <Text>Error Message: {error}</Text>}
